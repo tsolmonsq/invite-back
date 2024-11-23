34f1a83db3cb3146ccda323d38671e086d2b6a0c
tsolmonsq <tsolmonbatbold@admins-MacBook-Pro.local>
2024-11-23 14:54:56 +0800

    fix for vercel

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,16 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Invite')
-    .setDescription('The Invite management API documentation')
+  // Swagger configuration
+  const options = new DocumentBuilder()
+    .setTitle('Your API')
+    .setDescription('The API description')
     .setVersion('1.0')
-    .addTag('users')
     .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);  // Here, `/swagger` will be the Swagger UI endpoint
 
   await app.listen(3000);
 }
+
 bootstrap();
