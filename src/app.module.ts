e17a2a74859a7d8b3fa80fd44f2d133166ced425
tsolmonsq <tsolmonbatbold@admins-MacBook-Pro.local>
2024-11-23 14:51:30 +0800

    fix for vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { User } from './user/user.entity';
 import { databaseConfig } from './database/database.config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(databaseConfig),
     UserModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),  // Path to your 'public' folder
+    }),
   ],
 })
 export class AppModule {}
